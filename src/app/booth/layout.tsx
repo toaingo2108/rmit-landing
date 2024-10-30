19205e9f8166a5674e86d5f9109e0dc7288b4fc0
toainq <nqtoai2181@gmail.com>
2024-10-30 12:27:53 +0700

    fix booth layout

--- src/app/booth/layout.tsx
+++ src/app/booth/layout.tsx
@@ -1,44 +1,45 @@
+"use client";
+
 import Image from "next/image";
 import React, { Suspense } from "react";
 import Link from "next/link";
 import QRTooltip from "@/components/QRTooltip";
+import LinkWithId from "@/components/LinkWithId";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { KEY } from "@/constants/key";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
 }
 const BoothLayout = ({ children }: Props) => {
-  const headersList = headers();
-  const fullUrl = headersList.get("referer") || "";
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const key = searchParams.get("key");
+  const booth = +pathname.split("/booth/")[1] as keyof typeof KEY;
 
-  const data = fullUrl?.split("/booth/")[1]?.split("?key=") as
-    | [keyof typeof KEY, string]
-    | undefined;
-
-  const booth = data?.[0];
-  const key = data?.[1];
-
-  if (!key || !booth || !KEY[booth] || key !== KEY[booth]) {
-    redirect("/process");
+  if (!key || !KEY[booth] || key !== KEY[booth]) {
+    router.push("/process");
+    return <></>;
   }
 
   return (
     <div className="bg-register bg-no-repeat bg-cover h-full relative overflow-hidden flex flex-col">
       <div className="flex flex-col items-center pt-8 pb-6 px-6 relative z-[1]">
         <div className="grid grid-cols-2 w-full gap-3">
-          <Link href="/process">
-            <Image
-              src="/images/logo.png"
-              alt="logo"
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="w-full object-cover"
-              priority
-            />
-          </Link>
+          <LinkWithId to="/process">
+            <div className="select-none pointer-events-none">
+              <Image
+                src="/images/logo.png"
+                alt="logo"
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-full object-cover"
+                priority
+              />
+            </div>
+          </LinkWithId>
           <div className="flex justify-end space-x-1">
             <QRTooltip />
             <Link href="/scan-qr" className="w-1/4 flex-shrink-0 cursor-pointer">
@@ -66,9 +67,17 @@ const BoothLayout = ({ children }: Props) => {
           />
         </div>
       </div>
-      <Suspense>{children}</Suspense>
+      {children}
     </div>
   );
 };
 
-export default BoothLayout;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Suspense>
+      <BoothLayout>{children}</BoothLayout>
+    </Suspense>
+  );
+};
+
+export default Layout;
